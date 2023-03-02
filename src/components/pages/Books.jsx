349ada7dc1983e37d4495107ebd648a1626761bb
Carlos Eduardo Chanona Aquino <62265673+ShrkyChanona@users.noreply.github.com>
2023-03-02 12:30:32 -0600

    sprint 2 final

--- src/components/pages/Books.jsx
+++ src/components/pages/Books.jsx
@@ -1,6 +1,6 @@
 import Header from "../molecules/Header";
 import Footer from "../molecules/Footer";
-import RowCardsBooks from "../organism/RowCardsBooks";
+
 
 function Books() {
     return (
@@ -10,7 +10,7 @@ function Books() {
                     <Header />
                 </header>
                 <main>
-                    <RowCardsBooks></RowCardsBooks>
+                    
                 </main>
                 <footer>
                     <Footer />
